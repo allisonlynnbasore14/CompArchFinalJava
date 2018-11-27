89535d7d6a5b8522f3795f049d32880029ddcdde
Allison Basore <allison.basore@students.olin.edu>
2018-11-27 17:11:07 -0500

    Created SourceFile

---
+++ SourceFile.java
@@ -0,0 +1,23 @@
+public class sourceCode{
+
+    public sourceCode(){
+        
+        
+    int[] array = {0,1,2,3,4,5,6,7,8,9,10};
+    
+    int adder = 3;
+    
+        
+    for(int i =0; i<11;i++){
+        array[i] = array[i] + 3;
+        System.out.println(array[i]);
+    }
+    
+    return;
+        
+    }
+    
+     public static void main(String []args){
+        sourceCode sC = new sourceCode();
+     }
+}
