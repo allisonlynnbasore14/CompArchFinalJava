3427ce1433f8571352ccf0f526d13911143e6e23
Computer Architecture <allisonlynnbasore14@gmail.com>
2018-11-30 11:06:43 -0500

    added files

---
+++ pycparserReal/examples/exampleMin.c
@@ -0,0 +1,24 @@
+
+int main(void)
+{
+
+	int a = 12;
+	int b = 6;
+	// int c;
+
+	// c = minFunction(a, b);
+
+	return 0;
+}
+
+// int minFunction(int n1, int n2){
+
+// 	int min;
+// 	if(n1>n2){
+// 		min = n2;
+// 	}else{
+// 		min = n1;
+// 	}
+// 	return min;
+	
+// }
