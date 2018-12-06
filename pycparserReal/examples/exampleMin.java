c4aec48901e0ba129b2add550233888bb844f033
vickymmcd <vickymmcd@gmail.com>
2018-12-05 20:54:46 -0500

    pushing examplefunc.java

--- pycparserReal/examples/exampleMin.java
+++ pycparserReal/examples/exampleMin.java
@@ -4,18 +4,7 @@ public class ExampleMinNumber {
       int a = 212;
       int b = 6;
       int c = a+b;
-      //int c = minFunction(a, b);
 
    }
 
-   /** returns the minimum of two numbers */
-   // public static int minFunction(int n1, int n2) {
-   //    int min;
-   //    if (n1 > n2)
-   //       min = n2;
-   //    else
-   //       min = n1;
-
-   //    return min;
-   // }
 }
